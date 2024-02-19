e7f239f9acd3eb8961fdc4750b41c069c40b5e43
Divya kandpal <80275853+DivyaKandpal93@users.noreply.github.com>
2024-02-19 22:39:16 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 //your JS code here. If required.
 window.onload = function (){
 	const inputField = document.getElementById('fname');
-inputField.addEventListner('blur',(e) =>() {
+inputField.addEventListner('blur',(e) => () {
 	let inputValue = inputField.value();
 	let result = inputValue.toUpperCase();
 	inputField.value() = result;
