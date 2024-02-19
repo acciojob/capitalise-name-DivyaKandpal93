97f179e8afdcb379d8b68dea3b83b93cd5d6a419
Divya kandpal <80275853+DivyaKandpal93@users.noreply.github.com>
2024-02-19 22:31:52 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 //your JS code here. If required.
-window.onload = function (){
+// window.onload = function (){
 	const inputField = document.getElementById('fname');
 inputField.addEventListner(blur,function() {
 	let inputValue = inputField.value();
 	let result = inputValue.toUpperCase();
 	inputValue = result;
-})
+// })
