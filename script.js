4af2ddd6622e010efbd694166e65de66861a3f8f
Divya kandpal <80275853+DivyaKandpal93@users.noreply.github.com>
2024-02-19 22:21:54 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,14 +1,8 @@
 //your JS code here. If required.
-function resultedData(s){
+function (){
 	const inputField = document.getElementById('fname');
 inputField.addEventListner(blur,function() {
 	let inputValue = inputField.value();
 	let result = inputValue.toUpperCase();
 	inputField.value() = result;
-})
-}
-
-const s= prompt("Enter your name:");
-alert(resultedData(s));
-
 })
